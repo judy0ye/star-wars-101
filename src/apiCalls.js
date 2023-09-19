195d5924d78411dfaa98336c38740ae00e5d52ac
Judy Ye <judyy.ye@gmail.com>
2023-09-19 13:34:02 -0700

    Refactor GET request to fetch all characters due to API pagination

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,12 +1,27 @@
 function getCharacters() {
-  return fetch('https://swapi.dev/api/people/')
-  .then(res => {
-    if (!res.ok) {
-      throw new Error (`${res.status}: Unable to retrieve from server`)
-    }
-    return res.json()
-  })
+  let pageNum = 1
+  const characters = []
+
+  const fetchAllCharcters = () => {
+    return fetch(`https://swapi.dev/api/people/?page=${pageNum}`)
+    .then(res => {
+      if (!res.ok) {
+        throw new Error (`${res.status}: Unable to retrieve from server`)
+      }
+      return res.json()
+    })
+    .then(data => {
+      characters.push(...data.results)
+      if (data.next) {
+        pageNum++
+        return fetchAllCharcters()
+      }
+      return characters
+    })
+  }
+  return fetchAllCharcters()
 }
+
 
 export {
   getCharacters
