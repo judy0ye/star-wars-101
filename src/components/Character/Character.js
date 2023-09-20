b3f23d68843ae5423c1e874021d7898f8f9e2315
Judy Ye <judyy.ye@gmail.com>
2023-09-19 19:23:01 -0700

    Add Link to Character cards to visit selected CharacterDetails

--- src/components/Character/Character.js
+++ src/components/Character/Character.js
@@ -2,13 +2,13 @@ import './Character.css'
 import favorite from '../../assets/emojisky.com-16967011.png'
 import { Link } from 'react-router-dom'
 
-function Character({name, height, hairColor, skinColor, eyeColor, birthYear, gender}) {
+function Character({name, id, height, hairColor, skinColor, eyeColor, birthYear, gender}) {
   return (
     <div className='character-card'>
       <h2>{name}</h2>
      
       <div>
-        <Link>See more</Link>
+        <Link to={`/character/${id}`}>See more</Link>
         <img className='favorite' src={favorite}></img>
       </div>
     </div>
