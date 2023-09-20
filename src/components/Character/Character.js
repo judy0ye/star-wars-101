a52f78994b60abff1b370a5535090eb8105d9a02
Judy Ye <judyy.ye@gmail.com>
2023-09-20 11:20:26 -0700

    Add functionality to toggle opacity of favorite image upon click

--- src/components/Character/Character.js
+++ src/components/Character/Character.js
@@ -1,15 +1,29 @@
 import './Character.css'
 import favorite from '../../assets/emojisky.com-16967011.png'
 import { Link } from 'react-router-dom'
+import { useState } from 'react'
 
 function Character({name, id, height, hairColor, skinColor, eyeColor, birthYear, gender}) {
+  const [isFavorite, setIsFavorite] = useState(false)
+
+  const toggleFavorite= () => {
+    setIsFavorite(!isFavorite)
+  }
+
+  // const styleFavoriteImage = isFavorite ? 'favorite active' : 'favorite'
+  
+  const imgOpacity = isFavorite ? 0.9:.25
+  const styleFavoriteImage = {
+    opacity: imgOpacity 
+  }
   return (
     <div className='character-card'>
-      <h2>{name}</h2>
-     
+      <h2>{name}</h2> 
       <div>
         <Link to={`/character/${id}`}>See more</Link>
-        <img className='favorite' src={favorite}></img>
+        <button onClick={toggleFavorite}>
+          <img className='favorite' style={styleFavoriteImage} src={favorite}></img>
+        </button>
       </div>
     </div>
   )
