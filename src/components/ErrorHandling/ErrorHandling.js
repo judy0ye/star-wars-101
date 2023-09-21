a7a247614edb7485bed10ea59b355a3145a7ebc7
Judy Ye <judyy.ye@gmail.com>
2023-09-21 09:57:18 -0700

    Add conditional rendering for showing error or showing catch all path

--- src/components/ErrorHandling/ErrorHandling.js
+++ src/components/ErrorHandling/ErrorHandling.js
@@ -4,7 +4,8 @@ import errorImg from '../../assets/michael-marais-JLHyIwix46c-unsplash.jpg'
 function ErrorHandling({error}) {
   return (
     <div className='error'>
-      <h2>{error}</h2>
+      {error ? <h2>{error}</h2> : <h2>Sorry, this page does not exist</h2>}
+      <button>Retry</button>
       <img className='error-img' src={errorImg} />
     </div> 
   )
