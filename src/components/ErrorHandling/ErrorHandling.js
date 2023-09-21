f7d9f284e863203e62559f2c9e50d0e5f5f6d9e0
Judy Ye <judyy.ye@gmail.com>
2023-09-21 10:17:19 -0700

    Add Link back to main display from error page

--- src/components/ErrorHandling/ErrorHandling.js
+++ src/components/ErrorHandling/ErrorHandling.js
@@ -1,12 +1,13 @@
 import './ErrorHandling.css'
 import errorImg from '../../assets/michael-marais-JLHyIwix46c-unsplash.jpg'
+import { Link } from 'react-router-dom'
 
 function ErrorHandling({error}) {
   return (
     <div className='error'>
       {error ? <h2>{error}</h2> : <h2>Sorry, this page does not exist</h2>}
-      <button>Retry</button>
       <img className='error-img' src={errorImg} />
+      <Link className='retry' to={'/'}>Retry</Link>
     </div> 
   )
 }
